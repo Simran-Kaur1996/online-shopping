31888f279e7d036d2c89e9dd3053e82b2b69d4b2
Simran Kaur <simran31kaur@gmail.com>
2017-08-04 23:36:47 +0530

    commit

---
+++ shoppingbackend/src/main/java/com/grace/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.grace.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
